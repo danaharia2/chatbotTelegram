26aa4af07852c50a444450f68c0922d7b1867ba8
UrangAwak <94170679+danaharia2@users.noreply.github.com>
2025-10-11 06:37:38 +0700

    Update __init__.py

--- fiturBot/handlers/__init__.py
+++ fiturBot/handlers/__init__.py
@@ -1,11 +1,12 @@
 from .user_handlers import start, absen, status, test_connection, get_my_info, register
 from .admin_handlers import (
-    admin_stats, reset_attendance, force_attendance_check, export_data,
+    admin_stats, admin_help, reset_attendance, force_attendance_check, export_data,
     manual_kick, list_warnings, classroom_reminder_now, class_reminder_now, check_topics
 )
 
 __all__ = [
     'start', 'absen', 'status', 'test_connection', 'get_my_info', 'register',
-    'admin_stats', 'reset_attendance', 'force_attendance_check', 'export_data',
+    'admin_stats', 'admin_help', 'reset_attendance', 'force_attendance_check', 'export_data',
     'manual_kick', 'list_warnings', 'classroom_reminder_now', 'class_reminder_now', 'check_topics'
+
 ]
