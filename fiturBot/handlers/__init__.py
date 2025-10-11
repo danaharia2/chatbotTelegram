2e8698662aa716997d75579711cecfbcd059ebdc
UrangAwak <94170679+danaharia2@users.noreply.github.com>
2025-10-11 08:48:11 +0700

    Update __init__.py

--- fiturBot/handlers/__init__.py
+++ fiturBot/handlers/__init__.py
@@ -1,13 +1,11 @@
 from .user_handlers import start, absen, status, test_connection, get_my_info, register
 from .admin_handlers import (
-    admin_stats, admin_help, reset_attendance, force_attendance_check, export_data,
-    manual_kick, list_warnings, classroom_reminder_now, class_reminder_now, check_topics, test_classroom
+    admin_stats, reset_attendance, force_attendance_check, export_data, admin_help,
+    manual_kick, list_warnings, classroom_reminder_now, class_reminder_now, check_topics, admin_help, test_classroom, materi, materi1, materi2
 )
 
 __all__ = [
-    'start', 'absen', 'status', 'test_connection', 'get_my_info', 'register',
-    'admin_stats', 'admin_help', 'reset_attendance', 'force_attendance_check', 'export_data',
-    'manual_kick', 'list_warnings', 'classroom_reminder_now', 'class_reminder_now', 'check_topics', 'test_classroom'
-
+    'start', 'absen', 'status', 'test_connection', 'get_my_info', 'register', 'test_topic'
+    'admin_stats', 'admin_help', 'reset_attendance', 'force_attendance_check', 'export_data', 'admin_help'
+    'manual_kick', 'list_warnings', 'classroom_reminder_now', 'class_reminder_now', 'check_topics', 'test_classroom', 'materi', 'materi1', 'materi2'
 ]
-
