8525ae888b65d83ba2a077be526f2ab700a7d7e4
UrangAwak <94170679+danaharia2@users.noreply.github.com>
2025-10-14 21:47:44 +0700

    Update __init__.py

--- fiturBot/handlers/__init__.py
+++ fiturBot/handlers/__init__.py
@@ -4,7 +4,7 @@ from .admin_handlers import (
     classroom_reminder_now, class_reminder_now, check_topics, admin_help, test_classroom, start_auto_reminder, stop_auto_reminder, test_auto_reminder
 )
 from fiturBot.quiz_handler import (
-    quiz, quiz_callback_handler, handle_quiz_message,
+    start_command, help_command, quiz, quiz_callback_handler, handle_quiz_message,
     quiz_help, start_quiz, surrender_quiz, next_question, 
     show_score, show_points, top_score, quiz_rules, 
     quiz_donate, quiz_report, create_question_start
@@ -15,7 +15,7 @@ __all__ = [
     'admin_stats', 'admin_help', 'reset_attendance', 'force_attendance_check', 'export_data',
     'manual_kick', 'list_warnings', 'list_kehadiran', 'classroom_reminder_now', 'class_reminder_now', 'check_topics', 'test_classroom', 'materi', 'materi1', 'materi2', 'materi3', 'start_auto_reminder', 'stop_auto_reminder', 'test_auto_reminder', 'quiz_help',
     'create_question_start', 'get_all_member_ids', 'get_simple_member_ids',
-    'quiz',
+    'quiz', 'start_command', 'help_command',
     'start_quiz', 'quiz_rules', 'quiz_donate',
     'next_question', 'quiz_report',
     'quiz_callback_handler',
@@ -24,6 +24,7 @@ __all__ = [
     'handle_quiz_callback',
     'handle_quiz_message',
 ]
+
 
 
 
